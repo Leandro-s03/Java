28abc7ee7ce789dbb63d9dd9f1526a8ce175f8fe
Leandro <lealeaseguel@gmail.com>
2025-06-20 21:31:29 -0300

    Primer commit

---
+++ Persona.java
@@ -0,0 +1,31 @@
+public class Persona {
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public static void main(String[] args) {
+        Persona p1 = new Persona("Ana", 25);
+        System.out.println("Nombre: " + p1.getNombre());
+        System.out.println("Edad: " + p1.getEdad());
+    }
+}
